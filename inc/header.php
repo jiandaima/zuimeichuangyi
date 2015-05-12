4511952d45f956faaa2f6ef24f9153ab8d3941fe
Yang Zhongchao <yangzhongchao1011@gmail.com>
2015-05-12 22:16:02 +0800

    添加关键字
    添加关键字

--- inc/header.php
+++ inc/header.php
@@ -6,5 +6,8 @@
 	<link rel="shortcut icon" href="./favicon.ico" />
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
+	<meta name="keywords" content="最美创意,创意,最美,创意视频,视频,创意广告,最美创意网页版,最美创意电脑版,游戏CG,创意短片" />
+    <meta name="description" content="最美创意是一款以创意广告、游戏CG、创意短片为主的内容聚合型应用，它给喜爱这些内容的用户一个可以毫不费力找到他们的平台。" />
+    <meta name="author" content="羊种草" />
 </head>
 <body>
