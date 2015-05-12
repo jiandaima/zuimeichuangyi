75fea52b470ed1a2b0665a56ff7c3bdfded41d47
Yang Zhongchao <yangzhongchao1011@gmail.com>
2015-05-12 22:31:29 +0800

    添加统计代码
    添加统计代码

--- inc/footer.php
+++ inc/footer.php
@@ -14,5 +14,8 @@
     interval: 4000
     })
     </script>
+    <div style="display:none;">
+    <script type="text/javascript">
+    var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1255064204'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1255064204' type='text/javascript'%3E%3C/script%3E"));</script></div>
 </body>
 </html>
