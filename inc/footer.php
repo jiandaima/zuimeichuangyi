24df809a42dd66fbbf775b2eaa44ba7e752e132e
Yang Zhongchao <yangzhongchao1011@gmail.com>
2015-09-30 20:39:13 +0800

    修复统计代码
    修复统计代码

--- inc/footer.php
+++ inc/footer.php
@@ -2,18 +2,15 @@
     <hr>
         <p>最美创意 · 努力发现优质视频</p>
         <p>版权归<a target="_blank" href="http://www.bugkr.com/">虫客科技</a>所有</p>
-        <p><a target="_blank" href="https://github.com/yangzhongchao1011/zuimeichuangyi">github源码</a> · 制作<a target="_blank" href="http://www.yangzhongchao.com">羊种草</a></p>
+        <p><a target="_blank" href="https://github.com/yangzhongchao1011/zuimeichuangyi">github源码</a>
+         · 制作<a target="_blank" href="http://www.yangzhongchao.com">羊种草</a>
+    <script src="http://s11.cnzz.com/z_stat.php?id=1255064204&web_id=1255064204" language="JavaScript"></script>
+
+         </p>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script>
-    $('.carousel').carousel({
-    interval: 4000
-    })
-    <div style="display:none;">
-    <script type="text/javascript">
-    var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1255064204'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1255064204' type='text/javascript'%3E%3C/script%3E"));</script></div>
 </body>
 </html>
